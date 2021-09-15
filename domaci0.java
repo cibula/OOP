f11283ce60831818220185f22964e0c4f702e262
cibula <71884898+cibula@users.noreply.github.com>
2021-09-16 00:21:40 +0200

    Add files via upload

---
+++ domaci0.java
@@ -0,0 +1,20 @@
+public class domaci0{
+
+    public static void main(String[] args){
+
+        int a, b, c;
+        a = 7;
+        b = 3;
+        c = a + b;
+        String mali = "string", veliki = "STRING";
+        system.out.println("Zbir brojeva " + a + " i " + b + " je " + c +);
+
+        if(mali.equlas(veliki)){
+            system.out.println("Isti su");
+        }
+        else{
+            veliki.toLowerCase();
+        }
+
+    }
+}
